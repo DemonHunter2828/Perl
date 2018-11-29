f7e9d2cc5207eafa92d3f2b0eb74713d88831fe1
Fyodor <fed13579@yandex.ru>
2018-11-29 18:42:53 +0300

    Added cookie for VR requests limit

--- whois.pl
+++ whois.pl
@@ -2,21 +2,34 @@
 use strict;
 use URI::Escape;
 use warnings;
-use CGI;
+use CGI::Cookie;
 use lib "../../";
 use VR::Lib::RPC;
 use LWP::Protocol::https;
 use Template;
 
+if($ENV{HTTP_COOKIE})
+{
+    # nothing. we already have cookie
+}
+else
+{
+    my $idstr = uri_unescape($ENV{REMOTE_ADDR});
+    my $random = int(rand(1000000000));
+    my $cookieStr = "$random.$idstr";
+    my $c = new CGI::Cookie(-name => 'usrId',
+    -value => $cookieStr);
+    
+    print "Set-Cookie: $c\n";
+}
 print "Content-Type: text/html; charset=utf-8\n\n";
-my $domain_string;
-$domain_string = uri_unescape( substr $ENV{QUERY_STRING}, 5);
-my $idstr = uri_unescape( $ENV{REMOTE_ADDR});
+my $usrId = uri_unescape(substr $ENV{HTTP_COOKIE}, 6);
+my $domain_string = uri_unescape( substr $ENV{QUERY_STRING}, 5);
 my $server = "https://vrdemo.virtreg.ru/vr-api";
 my $login = "demo";
 my $password = "demo";
 my $rpc = VR::Lib::RPC->new($server, $login, $password);
-my %whois_hash = %{$rpc->call("whois",{auth=>{login=>$login, password=>$password}, name=>$domain_string, userCookie=>$idstr})};
+my %whois_hash = %{$rpc->call("whois",{auth=>{login=>$login, password=>$password}, name=>$domain_string, userCookie=>$usrId})};
 my $whois = $whois_hash{"result"}{"whois"};
 my $vars = {
     whois => $whois,
@@ -28,8 +41,10 @@ my $tt = Template->new({
 
 $tt->process('whois.tpl', $vars) || die $tt->error(), "\n";
 
-foreach my $hkey (sort {$a cmp $b} keys %ENV)
-{
-    print "$hkey:$ENV{$hkey}<br>";
-}
+#print "cookie:$ENV{HTTP_COOKIE}<br>";
+
+#foreach my $hkey (sort {$a cmp $b} keys %ENV)
+#{
+#    print "$hkey:$ENV{$hkey}<br>";
+#}
 
